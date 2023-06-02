1a97fbad4573c9ff38dd9179ef4c7e8c935857cd
Tania Strahan <thraell17@gmail.com>
2023-06-02 14:18:53 +1000

    add functionality to filter one first column, find matches with "and" keyword or find mismatches with "not" keyword

--- filterThings.py
+++ filterThings.py
@@ -1,43 +1,55 @@
 #!/usr/bin/env python3
+# filter two files, and return either matches, OR things in file 1 but NOT in file 2
+# use first column (delimited by tab) as key
  
 # --- LIBRARIES --- #
 from argparse import ArgumentParser
  
 # --- VARIABLES --- #
-HEAD_INDEX = 0
-PRON_INDEX = 1
  
 # --- OBJECTS ----- #
-class Entry():
-def __init__(self, head, pron, rank):
-	self.head = head
-	self.pron = pron
-	self.rank = rank
  
 # --- HELPER FUNCTIONS --- #
-# none yet
  
+def return_matches(file1, items_file2):
+	with open(file1, 'r') as inFile:
+		for line in inFile:
+			if line.strip().split("\t")[0] in items_file2:
+				print(line.strip())
+
+def return_missing(file1, items_file2):
+	with open(file1, 'r') as inFile:
+		for line in inFile:
+			if line.strip().split("\t")[0] not in items_file2:
+				print(line.strip())
+
+
 # --- MAIN --------------- #
 def main():
 	# create parser
 	parser = ArgumentParser(description = 'help text for script')
 	# add arguments to parser
-	parser.add_argument('lexicon', help='3-col lexicon')
-	parser.add_argument('-w', '--word', help='col of headword, default = 1', default = 1)
+	parser.add_argument('file1', help='reference file, output will be in this file')
+	parser.add_argument('operator', help='use "and" or "not" to decide which lines will be output')
+	parser.add_argument('file2', help='comparison file, output will be, or will NOT be, in this file')
 	# collate/parse arguments
 	args = parser.parse_args()
  	
-	# read in lexicon
-	lexicon = args.lexicon
-	currentLexicon = []
+	# read in files and operator
+	file1 = args.file1
+	file2 = args.file2
+	operator = args.operator
+	items_file2 = []
  	
-	with open(lexicon, 'r') as inFile:
+	with open(file2, 'r') as inFile:
 		for line in inFile:
-			currentLexicon.append(line.strip())
-			inFile.close()
+			items_file2.append(line.strip().split("\t")[0])
  	
-	print(currentLexicon)
+	if operator == "and":
+		return_matches(file1, items_file2)
+	if operator == "not":
+		return_missing(file1, items_file2)
  	
 if __name__ == "__main__":
-main()
+	main()
 
